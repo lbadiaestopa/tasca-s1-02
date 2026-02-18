8e307cd3320d40ac248f4fa16d399bcdb33c265c
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-02-18 16:03:14 +0100

    feat: add exercici 3a

---
+++ nivell-1/exercici3-a.php
@@ -0,0 +1,32 @@
+<?php
+
+    $x = 10;
+    $y = 3;
+
+    $n = 5.5;
+    $m = 2.2;
+
+    echo $x . "\n";
+    echo $y . "\n";
+    echo $x + $y . "\n";
+    echo $x - $y . "\n";
+    echo $x * $y . "\n";
+    echo $x % $y . "\n";
+
+    echo $n . "\n";
+    echo $m . "\n";
+    echo $n + $m . "\n";
+    echo $n - $m . "\n";
+    echo $n * $m . "\n";
+    echo $n % $m . "\n";
+
+    echo $x * 2 . "\n";
+    echo $y * 2 . "\n";
+    echo $n * 2 . "\n";
+    echo $m * 2 . "\n";
+
+    echo ($x + $y + $n + $m) . "\n";
+    
+    echo ($x * $y * $n * $m) . "\n";
+
+?>
