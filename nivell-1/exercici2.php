0dbe433b818b3bc31d003af2de8f152c1e1f4f46
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-02-18 16:01:53 +0100

    feat: add exercici 2

---
+++ nivell-1/exercici2.php
@@ -0,0 +1,15 @@
+<?php
+
+    $message = "Hello, World!";
+    echo $message . "\n";
+
+    echo strtoupper($message) . "\n";
+
+    echo strlen($message) . "\n";
+
+    echo strrev($message) . "\n";
+
+    $message2 = "Aquest es el curs de PHP";
+    echo $message . " " . $message2 . "\n";
+
+?>
