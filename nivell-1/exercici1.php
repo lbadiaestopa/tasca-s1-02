445e01cea434d824d97957f3b3c5490ead95869e
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-02-18 15:01:22 +0100

    feat: add exercici 1

---
+++ nivell-1/exercici1.php
@@ -0,0 +1,16 @@
+<?php
+
+    $integer = 10;
+    $double = 3.14;
+    $string = "Hello, World!";
+    $boolean = true;
+
+    echo $integer . "\n";
+    echo $double . "\n";
+    echo $string . "\n";
+    echo $boolean . "\n";
+
+    define("NAME", "Lluc");
+    echo NAME . "\n";
+ 
+?>
